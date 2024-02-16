314aabcf1b92b8c3a6f665ce0803296dd0f2abac
alej-mocse1 <alejandromocse01@gmail.com>
2024-02-15 23:55:24 -0300

    nuevo commit

--- app/page.js
+++ app/page.js
@@ -4,12 +4,14 @@ import styles from './page.module.css';
 import Colaborations from './components/colaborations/Colaborations.js';
 import Proyectos from './components/Proyectos/Proyectos.js';
 import Testimonials from './components/Testimonials/Testimonials.jsx';
+import localFont from 'next/font/local'
 import Footer from './components/footer/Footer.js';
+const myFont = localFont({ src: './font/AvertaDemoPE-ExtraBold.otf' })
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main  className={` ${styles.main} ${myFont.className}`}>
       <Presentation id="inicio"></Presentation>
       <AboutMe ></AboutMe>
       <Colaborations ></Colaborations>
