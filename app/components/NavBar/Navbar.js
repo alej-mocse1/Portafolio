cab3b414f567b871f8910a6d36322c238a90a604
alej-mocse1 <alejandromocse01@gmail.com>
2024-03-28 14:20:27 -0300

    cambios en la ux/ui

--- app/components/NavBar/Navbar.js
+++ app/components/NavBar/Navbar.js
@@ -4,9 +4,9 @@ import styles from "./NavBar.module.css";
 import Image from "next/image";
 import logo from "../../assets/logo.png";
 import logoWsp from "../../assets/logoWSP.jpg";
-import Close from '../../assets/close-line.png';
-import Open from '../../assets/menu-line.png';
-import Link from "next/link";  
+import Close from "../../assets/close-line.png";
+import Open from "../../assets/menu-line.png";
+import Link from "next/link";
 
 import { useState } from "react";
 
@@ -17,10 +17,10 @@ export default function NavBar() {
     <nav className={styles.nav}>
       <div className={styles.infoNav}>
         <div className={styles.divLogo}>
-{/*           
+          {/*           
           <div></div>
          */}
-          <Image src={logo} className={styles.imgLogo} /> 
+          <Image src={logo} className={styles.imgLogo} />
 
           <div
             className={styles.divTogle}
@@ -31,17 +31,21 @@ export default function NavBar() {
             {/* <span className={`${isOpen && styles.Span1}`}></span>
             <span className={`${isOpen && styles.Span2}`}></span>
             <span className={`${isOpen && styles.Span3}`}></span> */}
-            {isOpen ? 
-                  <Image src={Close}  className={styles.CloseTogle} /> :   <Image src={Open}   className={styles.IMGTogle}/> 
-            }
+            {isOpen ? (
+              <Image src={Close} className={styles.CloseTogle} />
+            ) : (
+              <Image src={Open} className={styles.IMGTogle} />
+            )}
           </div>
         </div>
 
         <div className={`${styles.divLinks} ${isOpen && styles.Open} `}>
           <ul>
-            <li >
-              <Link style={{borderBottom:'2px solid rgb(83, 177, 191)'}}  href="#inicio">Inicio</Link>
+            <li  className={`${styles.Inicio}`}  style={{color:"white"}}>
+              {/* <Link style={{borderBottom:'2px solid rgb(83, 177, 191)'}}  href="#inicio">Inicio</Link> */}
+              <Link href="#inicio" style={{textDecoration:"none", color:"white",borderBottom:'none'}}>Inicio</Link> 
             </li>
+
             <li>
               <Link href="#Proyectos">Proyectos</Link>
             </li>
@@ -62,7 +66,10 @@ export default function NavBar() {
       </div>
 
       <a>
-        <Link href="https://api.whatsapp.com/send?phone=2364556775" target="_blank">
+        <Link
+          href="https://api.whatsapp.com/send?phone=2364556775"
+          target="_blank"
+        >
           <Image src={logoWsp} className={styles.logoWsp} />
         </Link>
       </a>
--- app/components/Presentation/Presentation.js
+++ app/components/Presentation/Presentation.js
@@ -1,7 +1,7 @@
 "use client"
 import styles from './Presentation.module.css';
 import Image from 'next/image';
-import imgPrincp from '../../assets/JS.png'
+import imgPrincp from '../../assets/ts.jpg'
 import imgPrincp2 from '../../assets/react.png'
 import imgPrincp3 from '../../assets/Sql.jpg'
 import { GlowCapture, Glow } from '@codaworks/react-glow';
@@ -15,33 +15,17 @@ export default function Presentation() {
   useEffect(() => {
     Aos.init({ duration: 4000 });
   }, []);
-  return (
+  return (  
     <div className={styles.divPresentation}>
-        {/* <Image
-          //  width={360}
-          //  height={410}
-           src={imgPrincp}
-          className={styles.imgPrincp}
-        /> */}
 
-
-        {/* <h1>Developer <span>Full Stack</span></h1> */}
-        {/* <div data-aos="zoom-in-right"> */}
         <div data-aos="zoom-in-right"> 
         <h1>Developer <span><ReactTyped strings={["Full Stack"]} typeSpeed={110} /></span></h1>
          <p>Desarrollo de aplicaciones web de extremo a extremo de alta calidad.</p> 
-        {/* <p><ReactTyped strings={["Desarrollo de aplicaciones web de extremo a extremo de alta calidad."]} typeSpeed={40} /></p> */}
         </div>
 
 
-<div className={styles.DivImg} data-aos="zoom-in-right">
+<div className={styles.DivImg} >
 
-<Image
-          //   width={150}
-          //  height={150}
-           src={imgPrincp}
-          className={styles.imgPrincp}
-        />
 
 
   
@@ -52,7 +36,8 @@ export default function Presentation() {
           className={styles.imgPrincp2}
         />
 
-          <Image
+        
+<Image
           //   width={150}
           //  height={150}
            src={imgPrincp2}
@@ -60,8 +45,25 @@ export default function Presentation() {
         />
 
 
+<Image
+          //   width={150}
+          //  height={150}
+           src={imgPrincp}
+          className={styles.imgPrincp}
+        />
+
+
+
         </div> 
 
+
+<div className={styles.Contentdivision}>
+
+  <div>
+    
+  </div>
+
+</div>
  
     </div>
   )
