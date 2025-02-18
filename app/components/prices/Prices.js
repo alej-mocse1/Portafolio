cc73c00d99866482428eeaec86d6fd4a369bc752
Verónica De Luca <veronica.delucatalavera.externo@swissmedicalgroup.onmicrosoft.com>
2025-02-18 17:22:49 -0300

    reverse v 1.0.5

--- app/components/prices/Prices.js
+++ app/components/prices/Prices.js
@@ -13,7 +13,7 @@ export default function Prices() {
     <>
 
       <section className="bg-gray" id="Servicios" >
-        <div className="mx-auto  px-4 py-12 sm:px-6 md:py-16 lg:px-8">
+        <div className="mx-auto max-w-screen-xl px-4 py-12 sm:px-6 md:py-16 lg:px-8">
           <div className="mx-auto max-w-3xl text-center">
             <h2 className="text-3xl font-bold text-gray-900 sm:text-5xl">
             Desarrollo de aplicaciones de alta calidad
