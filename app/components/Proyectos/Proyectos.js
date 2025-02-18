3b4ef63940f8f83f624fbe42d779391f8d97c4b7
Verónica De Luca <veronica.delucatalavera.externo@swissmedicalgroup.onmicrosoft.com>
2025-02-18 16:51:15 -0300

    version 1.0.5

--- app/components/Proyectos/Proyectos.js
+++ app/components/Proyectos/Proyectos.js
@@ -33,7 +33,7 @@ export default function Proyectos() {
       </span>
 
       <section>
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+        <div className="mx-auto  px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
           <header className="text-center">
             <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
               Mis proyectos
